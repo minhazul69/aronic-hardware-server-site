bcbc68f87c930f59a7c50f73208ba1ee3e5d3a55
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-05-26 23:04:20 +0600

    error fix

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
     res.send(result);
   });
   //   FIND PRODUCT ON ID
-  app.get("/product/:id", verifyJWT, async (req, res) => {
+  app.get("/product/:id", async (req, res) => {
     const id = req.params.id;
     const query = { _id: ObjectId(id) };
     const result = await productCollection.findOne(query);
